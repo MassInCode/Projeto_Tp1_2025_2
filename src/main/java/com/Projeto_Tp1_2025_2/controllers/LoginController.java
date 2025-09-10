5292565db79244b9529398a18e8b59154e1eaa4d
Enzo Cardoso Martins <enzocm.26@gmail.com>
2025-09-09 22:32:03 -0300

    Slider e ChoiceBox
    Slider de salario e ChoiceBox de Cargo

--- src/main/java/com/Projeto_Tp1_2025_2/controllers/LoginController.java
+++ src/main/java/com/Projeto_Tp1_2025_2/controllers/LoginController.java
@@ -4,27 +4,45 @@ import javafx.scene.control.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
 
 import javafx.fxml.FXML;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
     @FXML private TextField tfNome;
     @FXML private TextField tfSenha;
     @FXML private Slider slSalario;
-    @FXML private ChoiceBox<String> cbCargo;
+    @FXML private ChoiceBox<String> cbCargos;
     @FXML private Button btnCadastro;
     @FXML private Button btnApagarBancodeDados;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cbCargos.getItems().add("Recrutador");
+        cbCargos.getItems().add("Gestor RH");
+        cbCargos.getItems().add("Administrador");
+        cbCargos.getItems().add("Funcionário");
+        cbCargos.setValue("Funcionário");
+    }
 
 
     @FXML private void btnCadastro(ActionEvent event) {
         String nome = tfNome.getText();
         String senha = tfSenha.getText();
+        String cargo = cbCargos.getValue();
+        int salario = (int) slSalario.getValue();
         try{
             FileWriter escrita = new FileWriter("data/usuarios.txt", true);
-            escrita.write(nome);
+            escrita.write("Nome: " + nome);
             escrita.write("\n");
-            escrita.write(senha);
+            escrita.write("Senha: " + senha);
+            escrita.write("\n");
+            escrita.write("Cargo: " + cargo);
+            escrita.write("\n");
+            escrita.write("Salario: " + salario);
             escrita.write("\n\n");
             escrita.close();
         } catch (IOException e){System.out.println("Erro ao criar escrita");}
