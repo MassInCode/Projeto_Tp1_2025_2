e1198331d4a31e839658e0dfa051f3efe0f8f5b6
Caio <caiodiasfleury@gmail.com>
2025-09-23 21:43:58 -0300

    ABLAbubu

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module Projeto_Tp1_2025_2 {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires javafx.graphics;
+    requires javafx.base;
 
     exports com.Projeto_Tp1_2025_2.main;
     opens com.Projeto_Tp1_2025_2.controllers to javafx.fxml;
