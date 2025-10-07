acd4bf611ec8f1bbc67eff276f350d6f00a10d9e
Caio <caiodiasfleury@gmail.com>
2025-10-07 20:33:31 -0300

    Update FolhaPagamento.java

--- src/main/java/com/Projeto_Tp1_2025_2/models/funcionario/FolhaPagamento.java
+++ src/main/java/com/Projeto_Tp1_2025_2/models/funcionario/FolhaPagamento.java
@@ -2,7 +2,11 @@ package com.Projeto_Tp1_2025_2.models.funcionario;
 
 public class FolhaPagamento {
 
-    void gerarFolhaPagamento()
+    public void gerarFolhaPagamento()
+    {
+
+    }
+    public void gerarRelatorioFinanceiro()7
     {
 
     }
