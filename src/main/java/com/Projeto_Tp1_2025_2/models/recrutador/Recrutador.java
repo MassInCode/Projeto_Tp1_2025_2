17021219c041902f5b429c36cd4420849b878243
IsaLeAlva <bella.lemos.bsb@gmail.com>
2025-10-23 14:02:14 -0300

    Começo da implementação recrutador

--- src/main/java/com/Projeto_Tp1_2025_2/models/recrutador/Recrutador.java
+++ src/main/java/com/Projeto_Tp1_2025_2/models/recrutador/Recrutador.java
@@ -7,26 +7,17 @@ import com.Projeto_Tp1_2025_2.models.candidatura.Candidato;
 import com.Projeto_Tp1_2025_2.models.funcionario.Funcionario;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Recrutador extends Funcionario {
-
     public Recrutador(String nome, String senha, String cpf, String email, String cargo, double salario, boolean status, String dataContratacao, String regime, String departamento) {
         super(nome, senha, cpf, email, cargo, salario, status, dataContratacao, regime, departamento);
     }
 
+    //erro loginController
     public Recrutador(String nome, String senha, String cpf, String email, String cargo) {
         super(nome, senha, cpf, email, cargo);
     }
 
-    public void criarVagas(){
-
-    }
-
-    public void gerenciarCandidatos(Vaga vaga){
-
-    }
-
-    public void solicitarContracao(Candidato candidato){
-
-    }
 }
--- src/main/java/com/Projeto_Tp1_2025_2/models/recrutador/Vaga.java
+++ src/main/java/com/Projeto_Tp1_2025_2/models/recrutador/Vaga.java
@@ -1,35 +1,82 @@
 package com.Projeto_Tp1_2025_2.models.recrutador;
 
+import com.Projeto_Tp1_2025_2.models.candidatura.Candidato;
+import com.Projeto_Tp1_2025_2.models.candidatura.Candidatura;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Vaga {
+
     private int id;
     private String cargo;
     private double salarioBase;
-    private ArrayList<String> requisitos = new ArrayList<>();
+    //private ArrayList<String> requisitos = new ArrayList<>();
+    private String requisitos;
+    private String regimeContratacao;
     private String departamento;
     private LocalDate dataAbertura;
+    private StatusVaga status;
+    private ArrayList<Candidato> candidatos;
 
-
-    public Vaga(int id, String cargo, double salarioBase, ArrayList<String> requisitos, String departamento, LocalDate dataAbertura) {
+    public Vaga(int id, String cargo, double salarioBase, String requisitos, String regimeContratacao, String departamento, LocalDate dataAbertura) {
         this.id = id;
         this.cargo = cargo;
         this.salarioBase = salarioBase;
-        this.requisitos.addAll(requisitos);
+        this.requisitos = requisitos;
+        this.regimeContratacao = regimeContratacao;
         this.departamento = departamento;
         this.dataAbertura = dataAbertura;
+        this.status = StatusVaga.ATIVO;
+
+        candidatos = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public String getRequisitos() {
+        return requisitos;
+    }
+
+    public String getRegimeContratacao() {
+        return regimeContratacao;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public StatusVaga getStatus() {
+        return status;
     }
 
     public void abrir(){
-
+        this.status = StatusVaga.ATIVO;
     }
 
     public void fechar(){
-
+        this.status = StatusVaga.FECHADA;
     }
 
-    public void editarVaga(String cargo, double salarioBase, String requisitos){
+    public void editarVaga(String cargo, double salarioBase, String requisitos, String regimeContratacao, String departamento){
+    }
 
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Cargo: " + cargo + ", Salário: " + salarioBase + ", Requisitos: " + requisitos + ", Departamento: " + departamento + ", Regime: " + regimeContratacao + ", Data de Abertura: " + dataAbertura + " ,Status: " + status;
     }
 }
