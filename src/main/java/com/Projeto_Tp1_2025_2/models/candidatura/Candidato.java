314a2bc3808745524a34e9de65a3545169210a29
Enzo Cardoso <enzocm.26@gmail.com>
2025-11-11 18:45:44 -0300

    Sla

--- src/main/java/com/Projeto_Tp1_2025_2/models/candidatura/Candidato.java
+++ src/main/java/com/Projeto_Tp1_2025_2/models/candidatura/Candidato.java
@@ -8,34 +8,21 @@ import com.Projeto_Tp1_2025_2.models.recrutador.Vaga;
 import java.util.ArrayList;
 import java.util.List;
 
+//CANDIDATO HERDANDO DE USUARIO
 public class Candidato extends Usuario {
 
     private String formacao;
-    //private List<Candidatura> candidaturas;
 
     //nao apagar
     public Candidato() {super();}
     //nao apagar
 
     public Candidato(String nome, String senha, String cpf, String email, String cargo, String formacao) throws InvalidPassword, InvalidCPF {
+        //CONSTRUTOR DE CANDIDATO PASSANDO OS PARAMETROS PARA A SUPER CLASSE USUARIO
         super(nome, senha, cpf, email, cargo);
         this.formacao = formacao;
-        //this.candidaturas = new ArrayList<>();
-    }
-
-    public boolean candidatar(Candidatura candidatura){
-        try{
-            //candidaturas.add(candidatura);
-            return true;
-        }
-        catch(Exception e){
-            System.out.println("Falha ao cadastrar Vaga");
-            return false;
-        }
     }
 
     public String getFormacao() {return formacao;}
     public void setFormacao(String formacao) {this.formacao = formacao;}
-    //public List<Candidatura> getCandidaturas() {return candidaturas;}
-
 }
